a4f059e155536f66f345a12151694ba17b9bdabd
oscargabriel <ozambranopinto@gmail.com>
2023-03-02 19:59:37 -0400

    añadiendo validacion de expreciones regulares para login y register

--- src/main/java/com/example/mongodb/controller/Impl/ControllerDefaultImpl.java
+++ src/main/java/com/example/mongodb/controller/Impl/ControllerDefaultImpl.java
@@ -4,8 +4,12 @@ package com.example.mongodb.controller.Impl;
 import com.example.mongodb.controller.ControllerDefault;
 import com.example.mongodb.dto.LoginUser;
 import com.example.mongodb.entities.User;
+import com.example.mongodb.exception.customizations.custom.InvalidExpressionException;
 import com.example.mongodb.service.Impl.UserServiceImpl;
 import com.example.mongodb.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
@@ -15,10 +19,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 //http://localhost:8080/tutorials
 @RestController
 public class ControllerDefaultImpl implements ControllerDefault {
+    Logger logger = LoggerFactory.getLogger(ControllerDefaultImpl.class);
+    private String regex = null;
+    private Pattern pat = null;
+    private Matcher math = null;
+
 
     UserService userService;
 
@@ -67,7 +78,14 @@ public class ControllerDefaultImpl implements ControllerDefault {
     @Override
     @PostMapping("/register")
     public ResponseEntity<User> register(@RequestBody User user) {
-        System.out.println("register");
+        logger.info("registrando usuario "+user.getUsername());
+        //validaciones que las expreciones regulares sean correctas
+        validarExprecionRegular(user.getEmail(), "email");
+        validarExprecionRegular(user.getUsername(), "username");
+        validarExprecionRegular(user.getPassword(), "password");
+        validarExprecionRegular(user.getPhone(), "phone");
+        validarExprecionRegular(user.getName(), "name");
+        validarExprecionRegular(user.getLastName(), "lastname");
         System.out.println(user.toString());
         return ResponseEntity.ok(userService.saveUser(user));
     }
@@ -82,7 +100,9 @@ public class ControllerDefaultImpl implements ControllerDefault {
     @Override
     @PostMapping("/authenticate")
     public ResponseEntity<?> authenticate(@RequestBody LoginUser loginUser) {
-        System.out.println("controller");
+        logger.info("autenticando a l usuario "+loginUser.getUsername());
+        validarExprecionRegular(loginUser.getPassword(), "password");
+        validarExprecionRegular(loginUser.getUsername(), "username");
         return ResponseEntity.ok(userService.authenticate(loginUser));
     }
 
@@ -94,5 +114,47 @@ public class ControllerDefaultImpl implements ControllerDefault {
     @Override
     public ResponseEntity<String> delete(Long id) {
         return null;
+    }
+
+    /**
+     * funcion para validar las expreciones regulares que se reciven desde autenticacion o registro
+     * si la cadena es aceptada continuara, de ser negativa se crea una expepcion InvalidExpressionExcepction
+     * y se envia al usuario el tipo que fallo
+     * @param cadena la cadena que va verificar (name, phone, email, password, username...)
+     * @param tipo el tipo de cadena que corresponde
+     */
+    protected void validarExprecionRegular(String cadena, String tipo){
+        switch (tipo) {
+            case "email":
+                regex = "^([A-Za-z0-9]{1})([-_\\.A-Za-z0-9]{0,})([A-Za-z0-9]{1})@([A-Za-z0-9\\.]+)*([A-Za-z]{2,})$";
+                break;
+            case "username":
+                regex = "^([A-Za-z0-9]{1})([A-Za-z0-9-_\\.]{2,20})([A-Za-z0-9]{1})$";
+                break;
+            case "password":
+                regex = "^([A-Za-z0-9-_@#%\\.\\+\\$\\*]{4,30})$";
+                break;
+            case "phone":
+                regex = "^([\\+]{0,1})([0-9]{3,})$";
+                break;
+            case "name":
+            case "lastname":
+                regex = "^([A-Za-z]{4,30})$";
+                break;
+            case "fecha":
+                regex = "^([0-9]{1,2})/([0-9]{1,2})/([0-9]{2,4})$";
+                break;
+
+            default:
+                regex=null;
+        }
+        if(regex!=null) {
+            Pattern pat = Pattern.compile(regex);
+            math = pat.matcher(cadena);
+            if(!math.find()){
+                throw new InvalidExpressionException(HttpStatus.PRECONDITION_FAILED,"el campo de " +tipo+" no es valido");
+            }
+        }
+
     }
 }
